39caaf599e8e1b143941bc5f3789746ce0408346
Rohan Usturge <rousturg@microsoft.com>
2018-01-21 22:23:23 +0530

    css and bug fixes

--- src/ts/eventhandlers/ExpandCollapseHandler.ts
+++ src/ts/eventhandlers/ExpandCollapseHandler.ts
@@ -11,20 +11,21 @@ export class ExpandCollapseHandler implements IEventHandler {
     constructor(private element: JQuery, private configStore: ConfigStore) {}
 
     public RegisterDomHandler(): void {
-        this.element.on("click", ".facet-body .expand-all", (event) => {
+        this.element.on("click", ".expand-all", (event) => {
             this.element.find(".facet-item-header").each((index, element) => {
                 this.ControlVisibilityOfFilter(jQuery(element), ShowHide.Show);
             });
             event.stopPropagation();
         });
-        this.element.on("click", ".facet-body .collapse-all", (event) => {
+        this.element.on("click", ".collapse-all", (event) => {
             this.element.find(".facet-item-header").each((index, element) => {
                 this.ControlVisibilityOfFilter(jQuery(element), ShowHide.Hide);
             });
             event.stopPropagation();
         });
-        this.element.on("click", ".facet-body .facet-item .facet-item-header", (event) => {
-            const headerElement = jQuery(event.target).parents(".facet-item-header");
+        this.element.on("click", ".facet-item-header", (event) => {
+            const target = jQuery(event.target);
+            const headerElement = target.is(".facet-item-header") ? target : target.parent(".facet-item-header");
             this.element.find(".facet-header .button").removeAttr("disabled");
             this.ControlVisibilityOfFilter(headerElement, ShowHide.Toggle);
             event.stopPropagation();
@@ -38,19 +39,19 @@ export class ExpandCollapseHandler implements IEventHandler {
     }
     private ControlVisibilityOfFilter = (element: JQuery, showHide: ShowHide) => {
         const iconElement = element.children("i");
-        const currentlyVisible = iconElement.hasClass("icon-accordion-expand");
+        const currentlyVisible = iconElement.hasClass("gui-icon-chevron-up");
         if (ShowHide.Toggle !== showHide && ((currentlyVisible && showHide === ShowHide.Show) ||
             (!currentlyVisible && showHide === ShowHide.Hide))) {
             return;
         }
         if (!currentlyVisible) {
             element.parents(".facet-item").find(".facet-item-description").slideDown();
-            iconElement.removeClass("icon-accordion-expand");
-            iconElement.addClass("icon-accordion-collapse");
+            iconElement.removeClass("gui-icon-chevron-down");
+            iconElement.addClass("gui-icon-chevron-up");
         } else {
             element.parents(".facet-item").find(".facet-item-description").slideUp();
-            iconElement.removeClass("icon-accordion-collapse");
-            iconElement.addClass("icon-accordion-expand");
+            iconElement.removeClass("gui-icon-chevron-up");
+            iconElement.addClass("gui-icon-chevron-down");
         }
     }
 }
