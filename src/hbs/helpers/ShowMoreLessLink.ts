a7aeab06fae4076724fcfd883de1dc3550e4577d
Rohan Usturge <rousturg@microsoft.com>
2018-04-25 03:24:34 +0530

    css fixes

--- src/hbs/helpers/ShowMoreLessLink.ts
+++ src/hbs/helpers/ShowMoreLessLink.ts
@@ -1,11 +1,11 @@
 const ShowMoreLessLink = (index, v2, options): string => {
     if (index.length > v2) {
-        return "<div class=\"gui m-t-10\">"
-              + "<a tabindex=\"0\" class=\"gui caps simple show-more-link cursor-pointer p-0 m-t-3\">"
-              + (index.length - v2)
-              + " More </a>"
-              + "<a tabindex=\"0\" class=\"gui caps simple show-less-link cursor-pointer gui-hidden p-0 m-t-3\">"
-              + "Show Less</a></div>";
+        return "<div class=\"gui m-b-5 m-t-5\">"
+        + "<a tabindex=\"0\" class=\"gui caps gui-body-2 simple show-more-link cursor-pointer p-0 m-t-3\">"
+        + (index.length - v2)
+        + " More </a>"
+        + "<a tabindex=\"0\" class=\"gui caps gui-body-2 simple show-less-link cursor-pointer gui-hidden p-0 m-t-3\">"
+        + "Show Less</a></div>";
     } else {
         return "";
     }
