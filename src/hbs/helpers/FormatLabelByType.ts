fa24e03317a3ba343be316acfa56f5dec9d59aa8
Rohan Usturge <rousturg@microsoft.com>
2018-05-25 14:59:57 +0530

    changing assigned to commited

--- src/hbs/helpers/FormatLabelByType.ts
+++ src/hbs/helpers/FormatLabelByType.ts
@@ -3,6 +3,12 @@ const formatLabelByType = (type: string, value: string) => {
     if (moment && type === "Edm.DateTimeOffset") {
         return moment.utc(value).format("DD-MMM-YYYY hh:mm a");
     } else {
+        if ( value === "Assigned" ) {
+            return "Committed";
+        }
+        if ( value === "Complete" ) {
+            return "Assigned";
+        }
         return value;
     }
 };
